e7d9be5c3197ac8fcf8eea1acfcfb713ef3f3460
Arthur Jamet <arthur.jamet@gmail.com>
2026-03-09 10:40:28 +0000

    Nixify

---
+++ shell.nix
@@ -0,0 +1,35 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in
+
+let
+  stack-wrapped = pkgs.symlinkJoin {
+    name = "stack";
+    paths = [ pkgs.stack ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/stack \
+        --add-flags "\
+          --nix \
+          --no-nix-pure \
+          --nix-shell-file=nix/stack-integration.nix \
+        "
+    '';
+  };
+
+in
+pkgs.mkShell {
+  buildInputs = [
+    stack-wrapped
+  ]
+  ++ (with pkgs; [
+    fourmolu
+    hlint
+    (haskell-language-server.override { supportedGhcVersions = [ "9103" ]; })
+    zlib
+    pkg-config
+  ]);
+
+  NIX_PATH = "nixpkgs=" + pkgs.path;
+}
